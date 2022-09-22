d0daca3205e09b50e2169acfc8e898e8b647bf34
Anurag Chemban <anurag.chemban@codezeros.com>
2022-09-22 11:44:29 +0530

    Hardhat upgrade Eg

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,21 +3,23 @@ require("@openzeppelin/hardhat-upgrades")
 require("@nomiclabs/hardhat-etherscan");
 
 
+require("dotenv").config()
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
 
   networks: {
     rinkeby: {
-      url: `https://rinkeby.infura.io/v3/441a81632bd64082a568a2d992c345ac`,
-      accounts: ['b191b1348b71e4a55425eefd7cef1e69a2a18456cabca4a12af77d396986bcf8']
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
     },
     localhost: {
       url: "http:127.0.0.1:7545"
     }
   },
   etherscan: {
-    apiKey:  "F6RX2N81GMURI1STC5GAWS15Y55QMXSPQA",
+    apiKey:  process.env.ETHERSCAN_API_KEY,
   }
 
 
